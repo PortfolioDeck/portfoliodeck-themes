3de1b4516297f40728d92ed91ecac9e89b0c6c5b
Martin Lissmyr <martin.lissmyr@oktavilla.se>
2012-06-07 13:34:11 +0200

    Added theme selection (in js file)

--- themulator/themulator.js
+++ themulator/themulator.js
@@ -1,5 +1,6 @@
 
 var loadPage = function(page) {
+  var theme = "Mariatorget";
   var ifrm = $("#content")[0];
   ifrm = (ifrm.contentWindow) ? ifrm.contentWindow : (ifrm.contentDocument.document) ? ifrm.contentDocument.document : ifrm.contentDocument;
   var html = "";
@@ -13,11 +14,11 @@ var loadPage = function(page) {
     cache: false
   });
   
-  $.get("theme/layout.liquid", function(layout) {
-    $.get("theme/" + page + ".liquid", function(pageContent) {
+  $.get("themes/" + theme + "/layout.liquid", function(layout) {
+    $.get("themes/" + theme + "/" + page + ".liquid", function(pageContent) {
       var src = layout.replace(/{{\s*?content_for_layout\s*?}}/, pageContent);
       var timeStamp = new Date().getTime();
-      src = src.replace(/\{\{\s*?theme\s*?\|\s*?asset_path:\s*?'(.*?)'\s*?\}\}/gi, "theme/$1?timestamp=" + timeStamp);
+      src = src.replace(/\{\{\s*?theme\s*?\|\s*?asset_path:\s*?'(.*?)'\s*?\}\}/gi, "themes/" + theme + "/assets/$1?timestamp=" + timeStamp);
       src = src.replace("</head>", "<meta http-equiv=\"Expires\" content=\"Tue, 01 Jan 2000 12:12:12 GMT\"><meta http-equiv=\"Pragma\" content=\"no-cache\"><base target=\"_top\"></head>");
       if (console) { console.log(src); }
       
