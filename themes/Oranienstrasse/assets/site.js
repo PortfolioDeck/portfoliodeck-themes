e5cde226ff653fed37d779ada6c1c7e1768d26d6
Martin Lissmyr <martin.lissmyr@oktavilla.se>
2012-06-13 16:12:58 +0200

    It's responsive!

--- themes/Oranienstrasse/assets/site.js
+++ themes/Oranienstrasse/assets/site.js
@@ -1,4 +1,4 @@
-$(function(){
+/*$(function(){
   var $container = $('#collection');
   $container.imagesLoaded(function(){
     $container.masonry({
@@ -6,4 +6,44 @@ $(function(){
       columnWidth : 200
     });
   });
-});
+});*/
+
+jQuery(document).ready(function($) {
+    var CollManag = (function() {
+        var $ctCollContainer = $('#collection'),
+        collCnt = 1,
+        init = function() {
+            changeColCnt();
+            initEvents();
+            initPlugins();
+        },
+        changeColCnt = function() {
+            var w_w = $(window).width();
+            if( w_w <= 768 ) n = 1;
+            else n = 2;
+        },
+        initEvents = function() {
+            $(window).on( 'smartresize.CollManag', function( event ) {
+                changeColCnt();
+            });
+        },
+        initPlugins = function() {
+            $ctCollContainer.imagesLoaded( function(){
+                $ctCollContainer.masonry({
+                    itemSelector : '.item',
+                    columnWidth : function( containerWidth ) {
+                        return containerWidth / n;
+                    },
+                    isAnimated : true,
+                    animationOptions: {
+                        duration: 400
+                    }
+                });
+            });
+            $ctCollContainer.colladjust();
+            $ctCollContainer.find('div.ct-coll-item-multi').collslider();
+        };
+        return { init: init };
+    })();
+    CollManag.init();
+}); 
